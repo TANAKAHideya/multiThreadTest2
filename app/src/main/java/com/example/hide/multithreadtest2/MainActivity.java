3255e9d72c2e4e44bb5d5036c7d833cb7fcb5645
TANAKA Hideya <hideya.tanaka.dev@gmail.com>
2018-09-02 23:11:03 +0900

    update

--- app/src/main/java/com/example/hide/multithreadtest2/MainActivity.java
+++ app/src/main/java/com/example/hide/multithreadtest2/MainActivity.java
@@ -6,9 +6,8 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Used to load the 'native-lib' library on application startup.
     static {
-        System.loadLibrary("native-lib");
+        System.loadLibrary("sum-jni");
     }
 
     @Override
@@ -16,14 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    public native String sumFromJNI();
 }
