e8b0a1f309d0c94741e8befcab815106a2042d52
TANAKA Hideya <hideya.tanaka.dev@gmail.com>
2018-09-03 16:33:41 +0900

    first edition

---
+++ app/src/main/cpp/native-lib.c
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) 2009 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <jni.h>
+#include <stdio.h>
+#include <android/log.h>
+#include <pthread.h>
+
+/* This is a trivial JNI example where we use a native method
+ * to return a new VM String. See the corresponding Java source
+ * file located at:
+ *
+ *   apps/samples/hello-jni/project/src/com/example/HelloJni/HelloJni.java
+ */
+
+#define MAX_THREADS 16
+
+long long sum_func[MAX_THREADS];
+
+struct param {
+    int endNum;
+    int tno;
+};
+
+long long common_func(int x){
+    int i;
+    long long sum=0;
+    for(i = 1;i<=x;i++){
+        sum += i;
+    }
+//	__android_log_print(ANDROID_LOG_ERROR,"sum-jni","common_func %ld %ld\n",i,sum);
+    return sum;
+}
+
+void func( struct param * x) {
+    sum_func[x->tno] = common_func(x->endNum);
+//	__android_log_print(ANDROID_LOG_ERROR,"sum-jni","func %x\n",(unsigned int)x);
+//	__android_log_print(ANDROID_LOG_ERROR,"sum-jni","func %d %d %lld\n",x->tno, x->endNum, sum_func[x->tno]);
+}
+
+JNIEXPORT jlong JNICALL
+Java_com_example_hide_multithreadtest2_MainActivity_sumFromJNI (JNIEnv* env, jobject thiz, jint endNum, jint tno, jboolean debug)
+{
+    struct param param[MAX_THREADS];
+    pthread_t t[MAX_THREADS];
+    int i;
+    jlong all_sum=0;
+
+    if(tno>MAX_THREADS){
+        return -1;
+    }
+
+    for(i=0;i<tno;i++) {
+        param[i].endNum = debug ? endNum + i: endNum;
+        param[i].tno = i;
+//		__android_log_print(ANDROID_LOG_ERROR,"Hello","init %d %d %x\n",param[i].tno, param[i].endNum,(unsigned int)(param+i));
+    };
+    for(i=0;i<tno;i++) {
+        pthread_create( &t[i], NULL, (void *)func, &param[i]);
+    }
+    for(i=0;i<tno;i++) {
+        pthread_join( t[i], NULL );
+    }
+    for(i=0;i<tno;i++) {
+        all_sum += sum_func[i];
+    }
+    return all_sum;
+}
--- app/src/main/java/com/example/hide/multithreadtest2/MainActivity.java
+++ app/src/main/java/com/example/hide/multithreadtest2/MainActivity.java
@@ -2,28 +2,132 @@ package com.example.hide.multithreadtest2;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
-    // Used to load the 'native-lib' library on application startup.
+        public static final int MAX_THREADS=16; /* up to 16 */
+        View changeButton,clearButton,checkBox;
+        boolean debugmode = false;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.activity_main);
+
+        changeButton = this.findViewById(R.id.Button01);
+        changeButton.setOnClickListener((android.view.View.OnClickListener) this);
+        clearButton = this.findViewById(R.id.Button02);
+        clearButton.setOnClickListener((android.view.View.OnClickListener) this);
+        checkBox = this.findViewById(R.id.checkBox1);
+        ((CompoundButton) checkBox).setChecked(false);
+        checkBox.setOnClickListener((android.view.View.OnClickListener) this);
+
+    }
+
+    public void onClick(View v) {
+        View edtEnd = this.findViewById(R.id.EditText01);
+        String strEnd = ((TextView) edtEnd).getText().toString();
+        if(v==changeButton){
+            {
+                String strSum;
+                long sUpTimeMillis = SystemClock.uptimeMillis();
+
+                long sum = sumFromJNI(Integer.valueOf(strEnd),MAX_THREADS,debugmode);
+                strSum = String.valueOf(sum);
+
+                long eUpTimeMillis = SystemClock.uptimeMillis();
+
+                View txtResult = this.findViewById(R.id.TextView04);
+                ((TextView)txtResult).setText(strSum);
+
+                View elpTime = this.findViewById(R.id.TextView05);
+                ((TextView)elpTime).setText("Native : " + String.valueOf(eUpTimeMillis - sUpTimeMillis) + " ms");
+            } /* */
+
+            {
+                String strSum;
+                long sUpTimeMillis = SystemClock.uptimeMillis();
+
+                if(!strEnd.equals("")){
+                    long numEnd = Long.valueOf(strEnd);
+                    long sum = 0;
+
+                    Count[] thread = new Count[MAX_THREADS];
+
+                    for(int i=0;i<MAX_THREADS;i++){
+                        thread[i] = new Count(debugmode ? numEnd + i : numEnd);
+                    }
+                    for(int i=0;i<MAX_THREADS;i++){
+                        thread[i].start();
+                    }
+                    try {
+                        for(int i=0;i<MAX_THREADS;i++){
+                            thread[i].join();
+                        }
+                    } catch(Exception ex) {
+                        ex.printStackTrace();
+                    }
+
+                    for(int i=0;i<MAX_THREADS;i++){
+                        sum += thread[i].getSum();
+                    }
+                    strSum = String.valueOf(sum);
+                } else {
+                    strSum = String.valueOf(0);
+                }
+                long eUpTimeMillis = SystemClock.uptimeMillis();
+
+                View txtResult = this.findViewById(R.id.TextView02);
+                ((TextView)txtResult).setText(strSum);
+
+                View elpTime = this.findViewById(R.id.TextView03);
+                ((TextView)elpTime).setText("DalVik : " + String.valueOf(eUpTimeMillis - sUpTimeMillis) + " ms");
+            } /* */
+        } else if(v==clearButton) {
+            {
+                View elpTime = this.findViewById(R.id.TextView05);
+                ((TextView)elpTime).setText("Native : 0 ms");
+            }
+            {
+                View elpTime = this.findViewById(R.id.TextView03);
+                ((TextView)elpTime).setText("DalVik : 0 ms");
+            }
+        } else {
+            if((boolean)( ((CompoundButton) checkBox).isChecked() ) == false){
+                debugmode = false;
+            } else {
+                debugmode = true;
+            }
+        }
+    }
+
+    public native long  sumFromJNI(int nNumber, int tno, boolean debugmode);
     static {
         System.loadLibrary("native-lib");
     }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+}
+class Count extends Thread {
+    private long endNum;
+    private long sum;
+    public Count(long x){
+        endNum=x;
     }
-
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    public void run() {
+        sum =0;
+        for(int i=0;i<=endNum;i++){
+            sum +=i;
+        }
+    }
+    public long getSum(){
+        return sum;
+    }
 }
